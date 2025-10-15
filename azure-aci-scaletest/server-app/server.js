300e2c8162df86129aeb062469ce15794b72db88
dazdaz <darren@dazdaz.org>
2025-10-15 13:11:12 +0200

    Moved contents of https://github.com/dazdaz/azure-aci-scaletest.git into azure-aci-scaletest/

---
+++ azure-aci-scaletest/server-app/server.js
@@ -0,0 +1,25 @@
+
+const express = require('express') 
+const logger = require('morgan')
+const errorhandler = require('errorhandler')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+
+let app = express()
+
+app.use(bodyParser.json())
+app.use(logger('dev'))
+app.use(errorhandler())
+/*app.use((req, res, next) => {
+  if (!req.store)
+    req.store = store
+  next()
+})*/
+
+app.get('/results', routes.getResults)
+app.post('/flush', routes.flushStore)
+app.post('/timestamp', routes.addTimestamp)
+app.post('/controller', routes.controller)
+
+app.listen(3000)
